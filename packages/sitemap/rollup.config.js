37773528048a3e9ff47b20d108d9144d1ab3535f
Mr.Hope <zhangbowang1998@gmail.com>
2021-01-31 11:54:57 +0800

    feat: add sitemap plugin

---
+++ packages/sitemap/rollup.config.js
@@ -0,0 +1,20 @@
+import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true, exports: "named" },
+    ],
+    plugins: [typescript(), terser()],
+    external: ["@vuepress/core", "chalk", "fs-extra", "path", "sitemap"],
+  },
+  {
+    input: "./src/index.ts",
+    output: [{ file: pkg.types, format: "esm", sourcemap: true }],
+    plugins: [dts()],
+  },
+];
