45b170a168016bef1059e88fd83f27a03bf7ef8c
Mr.Hope <zhangbowang1998@gmail.com>
2021-03-10 20:14:31 +0800

    feat(theme): update Anchor

--- packages/theme/src/components/Anchor.vue
+++ packages/theme/src/components/Anchor.vue
@@ -1,18 +1,20 @@
 <script lang="ts">
 import { h } from "vue";
-import type { FunctionalComponent, VNode, PropType } from "vue";
-import { SidebarHeader, groupSidebarHeaders } from "../util/sidebar";
-import { RouteLocation, useRoute } from "vue-router";
+import { usePageData } from "@vuepress/client";
+import { useRoute } from "vue-router";
 import { isActive } from "../utils";
-import { usePageFrontmatter, useThemeLocaleData } from "@vuepress/client";
+
+import type { FunctionalComponent, VNode } from "vue";
+import type { SidebarHeader } from "../util/sidebar";
 
 interface AnchorItem {
-  link: string;
   text: string;
+  level?: number;
+  link: string;
   active: boolean;
 }
 
-const renderLink = ({ text, link, active }: AnchorItem): VNode =>
+const renderLink = ({ text, link, level, active }: AnchorItem): VNode =>
   h(
     "RouterLink",
     {
@@ -22,46 +24,27 @@ const renderLink = ({ text, link, active }: AnchorItem): VNode =>
       class: {
         active,
         "anchor-link": true,
+        [level ? `heading${level}` : ""]: level,
       },
     },
-    h("div", {}, text)
+    h("div", text)
   );
 
-interface RenderChildrenOptions {
-  children: SidebarHeader[] | false;
-  path: string;
-  route: RouteLocation;
-  depth?: number;
-  maxDepth: number;
-}
-
-const renderChildren = ({
-  children,
-  path,
-  route,
-  maxDepth,
-  depth = 2,
-}: RenderChildrenOptions): VNode | null => {
-  if (!children || depth > maxDepth) return null;
+const renderChildren = (header: SidebarHeader[]): VNode => {
+  const route = useRoute();
 
   return h(
     "ul",
     { class: "anchor-list" },
-    children.map((child: SidebarHeader) => {
-      const active = isActive(route, `${path}#${child.slug}`);
+    header.map((child: SidebarHeader) => {
+      const active = isActive(route, `${route.path}#${child.slug}`);
 
-      return h("li", { class: ["anchor", `anchor${depth}`] }, [
+      return h("li", { class: "anchor" }, [
         renderLink({
           text: child.title,
-          link: `${path}#${child.slug}`,
+          link: `${route.path}#${child.slug}`,
+          level: child.level,
           active,
-        }),
-        renderChildren({
-          children: child.children || false,
-          path,
-          route,
-          maxDepth,
-          depth: depth + 1,
         }),
       ]);
     })
@@ -69,146 +52,150 @@ const renderChildren = ({
 };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const Anchor: FunctionalComponent<{ header: SidebarHeader[] }> = ({
-  header = [],
-}) => {
-  const pageFrontmatter = usePageFrontmatter();
-  const themeLocaleData = useThemeLocaleData();
-  const route = useRoute();
-  const maxDepth =
-    (pageFrontmatter.value.sidebarDepth ||
-      (themeLocaleData.value.sidebarDepth as number | undefined) ||
-      themeLocaleData.value.sidebarDepth ||
-      2) + 1;
-  const children = groupSidebarHeaders(header);
+const Anchor: FunctionalComponent = () => {
+  const page = usePageData();
 
-  return h("div", { attrs: { class: "anchor-place-holder" } }, [
-    h("aside", { attrs: { id: "anchor" } }, [
-      h("div", { class: "anchor-wrapper" }, [
-        renderChildren({
-          children,
-          path: route.path,
-          route,
-          maxDepth,
-        }),
-      ]),
-    ]),
-  ]);
+  return h(
+    "div",
+    { class: "anchor-place-holder" },
+    h(
+      "aside",
+      { id: "anchor" },
+      h(
+        "div",
+        { class: "anchor-wrapper" },
+        page.value.headers ? renderChildren(page.value.headers) : []
+      )
+    )
+  );
 };
 
 Anchor.displayName = "Anchor";
 
-Anchor.props = {
-  header: {
-    type: Array as PropType<SidebarHeader[]>,
-    default: (): SidebarHeader[] => [],
-  },
-};
-
 export default Anchor;
 </script>
 
-<style lang="stylus">
-@require '../styles/palette'
+<style lang="scss">
+.anchor-place-holder {
+  position: sticky;
+  top: ($navbarHeight + 2rem);
+  max-width: $contentWidth;
+  margin: 0 auto;
+  padding: 0 2.5rem;
 
-$headings = 2 3 4 5 6
+  @media (max-width: $MQNarrow) {
+    padding: 0 1.5rem;
+  }
 
-.anchor-place-holder
-  position sticky
-  top ($navbarHeight + 2rem)
-  max-width $contentWidth
-  margin 0 auto
-  padding 0 2.5rem
+  & + .theme-default-content:not(.custom) {
+    padding-top: 0;
+  }
+}
 
-  @media (max-width $MQNarrow)
-    padding 0 1.5rem
+#anchor {
+  display: none;
+  position: absolute;
+  left: calc(100% + 0.5rem);
+  min-width: 10rem;
+  max-width: 15rem;
+  max-height: 80vh;
+  overflow-y: scroll;
 
-  & + .theme-default-content:not(.custom)
-    padding-top 0
+  @media (min-width: $MQWide) {
+    .has-anchor & {
+      display: block;
+    }
+  }
 
-#anchor
-  display none
-  position absolute
-  left calc(100% + 0.5rem)
-  min-width 10rem
-  max-width 15rem
-  max-height 80vh
-  overflow-y scroll
+  &::-webkit-scrollbar-track-piece {
+    background: transparent;
+  }
 
-  @media (min-width $MQWide)
-    .has-anchor &
-      display block
+  &::-webkit-scrollbar {
+    width: 3px;
+  }
 
-  &::-webkit-scrollbar-track-piece
-    background transparent
+  &::-webkit-scrollbar-thumb:vertical {
+    background: #ddd;
 
-  &::-webkit-scrollbar
-    width 3px
+    .theme-dark & {
+      background: #333;
+    }
+  }
 
-  &::-webkit-scrollbar-thumb:vertical
-    background var(--light-grey)
+  .anchor-wrapper {
+    position: relative;
+    padding-left: 8px;
 
-  .anchor-wrapper
-    position relative
-    padding-left 8px
+    &::after {
+      content: " ";
+      position: absolute;
+      top: 15px;
+      left: 4px;
+      bottom: 15px;
+      width: 4px;
+      margin-left: -2px;
+      background: var(--border-color);
+      z-index: -1;
+    }
 
-    &::after
-      content ' '
-      position absolute
-      top 15px
-      left 4px
-      bottom 15px
-      width 4px
-      margin-left -2px
-      background var(--border-color)
-      z-index -1
+    > .anchor-list {
+      margin: 0;
+    }
+  }
 
-    > .anchor-list
-      margin 0
+  .anchor-list {
+    padding-left: 0;
+  }
 
-  .anchor-list
-    padding-left 0
+  .anchor {
+    box-sizing: border-box;
+    padding: 0px 8px;
+    line-height: 1.5;
+    list-style: none;
 
-  .anchor
-    box-sizing border-box
-    padding 0px 8px
-    line-height 1.5
-    list-style none
+    .anchor-link {
+      display: inline-block;
+      position: relative;
+      max-width: 100%;
+      color: var(--light-grey);
 
-    .anchor-link
-      display inline-block
-      position relative
-      max-width 100%
-      color var(--light-grey)
+      > div {
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+      }
 
-      > div
-        text-overflow ellipsis
-        white-space nowrap
-        overflow hidden
+      &::before {
+        content: " ";
+        position: absolute;
+        z-index: 2;
+        top: 50%;
+        left: -16px;
+        width: 6px;
+        height: 6px;
+        background: var(--bgcolor);
+        border: 1px solid var(--border-color);
+        border-radius: 50%;
+        transform: translateY(-3px);
+      }
 
-      &::before
-        content ' '
-        position absolute
-        z-index 2
-        top 50%
-        width 6px
-        height 6px
-        margin-left -3px
-        margin-top -3px
-        background var(--bgcolor)
-        border 1px solid var(--border-color)
-        border-radius 50%
+      &:hover,
+      &.active {
+        color: var(--accent-color);
 
-      &:hover, &.active
-        color var(--accent-color)
+        &::before {
+          background: var(--accent-color);
+        }
+      }
 
-        &::before
-          background var(--accent-color)
-
-    for $heading in $headings
-      &.anchor{$heading} .anchor-link
-        font-size (16 - $heading)px
-
-        &::before
-          left (-8 * $heading + 4)px
+      @for $heading from 2 to 6 {
+        &.heading#{$heading} {
+          padding-left: ($heading * 8 - 16) px;
+          font-size: (16 - $heading) px;
+        }
+      }
+    }
+  }
+}
 </style>
