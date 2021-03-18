a0e340c445d3427a3fb906c2ebca7cb1678b949a
Mr.Hope <zhangbowang1998@gmail.com>
2021-03-18 20:45:17 +0800

    feat: update theme

--- packages/theme-test/src/components/Navbar.vue
+++ packages/theme-test/src/components/Navbar.vue
@@ -1,47 +1,85 @@
 <template>
-  <header ref="navbar" class="navbar">
+  <header ref="navbar" class="navbar" :class="{ 'can-hide': canHide }">
     <ToggleSidebarButton @toggle="$emit('toggle-sidebar')" />
 
-    <span ref="siteBrand">
-      <RouterLink :to="siteBrandLink">
-        <img
-          v-if="siteBrandLogo"
-          class="logo"
-          :src="$withBase(siteBrandLogo)"
-          :alt="siteBrandTitle"
-        />
-
-        <span
-          v-if="siteBrandTitle"
-          class="site-name"
-          :class="{ 'can-hide': siteBrandLogo }"
-        >
-          {{ siteBrandTitle }}
-        </span>
-      </RouterLink>
-    </span>
+    <RouterLink ref="siteBrand" :to="siteBrandLink" class="home-link">
+      <img
+        v-if="siteBrandLogo"
+        class="logo"
+        :class="{ light: Boolean(siteBrandDarkLogo) }"
+        :src="siteBrandLogo"
+        :alt="siteBrandTitle"
+      />
+      <img
+        v-if="siteBrandDarkLogo"
+        class="logo dark"
+        :src="siteBrandDarkLogo"
+        :alt="siteBrandTitle"
+      />
+      <span
+        v-if="siteBrandTitle"
+        class="site-name"
+        :class="{ 'can-hide': siteBrandLogo }"
+      >
+        {{ siteBrandTitle }}
+      </span>
+    </RouterLink>
 
     <div class="navbar-links-wrapper" :style="linksWrapperStyle">
       <slot name="before" />
+      <!-- <ThemeColor /> -->
       <NavbarLinks class="can-hide" />
       <slot name="after" />
       <Docsearch />
+      <!-- <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algoliaConfig" /> -->
+      <!-- <SearchBox
+        v-else-if="
+          $themeConfig.search !== false && $page.frontmatter.search !== false
+        "
+      /> -->
     </div>
   </header>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, onMounted, ref } from "vue";
-import { useRouteLocale, useSiteLocaleData } from "@vuepress/client";
-import { useThemeLocaleData } from "../composables";
+import { useRouteLocale, useSiteLocaleData, withBase } from "@vuepress/client";
+import { useThemeLocaleData } from "@vuepress/plugin-theme-data/lib/composables";
 import NavbarLinks from "./NavbarLinks.vue";
+// import ThemeColor from "./Theme/ThemeColor.vue";
 import ToggleSidebarButton from "./ToggleSidebarButton.vue";
+
+import "./Navbar.scss";
+
+import type { HopeThemeOptions } from "../types";
+
+const getCssValue = (
+  el: Element | null,
+  property: keyof Omit<
+    CSSStyleDeclaration,
+    | "getPropertyPriority"
+    | "getPropertyValue"
+    | "item"
+    | "removeProperty"
+    | "setProperty"
+    | number
+  >
+): number => {
+  // NOTE: Known bug, will return 'auto' if style value is 'auto'
+  const value = el?.ownerDocument.defaultView?.getComputedStyle(el, null)[
+    property
+  ] as string;
+
+  const num = Number.parseInt(value, 10);
+  return Number.isNaN(num) ? 0 : num;
+};
 
 export default defineComponent({
   name: "Navbar",
 
   components: {
     NavbarLinks,
+    // ThemeColor,
     ToggleSidebarButton,
   },
 
@@ -50,23 +88,37 @@ export default defineComponent({
   setup() {
     const routeLocale = useRouteLocale();
     const siteLocale = useSiteLocaleData();
-    const themeLocale = useThemeLocaleData();
+    const themeLocale = useThemeLocaleData<HopeThemeOptions>();
+
+    const isMobile = ref(false);
 
     const navbar = ref<HTMLElement | null>(null);
     const siteBrand = ref<HTMLElement | null>(null);
+
     const siteBrandLink = computed(
       () => themeLocale.value.home || routeLocale.value
     );
-    const siteBrandLogo = computed(() => themeLocale.value.logo);
+    const siteBrandLogo = computed(() =>
+      themeLocale.value.logo ? withBase(themeLocale.value.logo) : null
+    );
+    const siteBrandDarkLogo = computed(() =>
+      themeLocale.value.darkLogo ? withBase(themeLocale.value.darkLogo) : null
+    );
     const siteBrandTitle = computed(() => siteLocale.value.title);
+
     const linksWrapperMaxWidth = ref(0);
     const linksWrapperStyle = computed(() => {
-      if (!linksWrapperMaxWidth.value) {
-        return {};
-      }
+      if (!linksWrapperMaxWidth.value) return {};
+
       return {
-        "max-width": linksWrapperMaxWidth.value + "px",
+        "max-width": `${linksWrapperMaxWidth.value}px`,
       };
+    });
+
+    const canHide = computed(() => {
+      const autoHide = themeLocale.value.navAutoHide;
+
+      return autoHide !== "none" && (autoHide === "always" || isMobile.value);
     });
 
     // avoid overlapping of long title and long navbar links
@@ -79,9 +131,12 @@ export default defineComponent({
         getCssValue(navbar.value, "paddingRight");
       const handleLinksWrapWidth = (): void => {
         if (window.innerWidth < MOBILE_DESKTOP_BREAKPOINT) {
+          isMobile.value = true;
           linksWrapperMaxWidth.value = 0;
         } else {
+          isMobile.value = false;
           linksWrapperMaxWidth.value =
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             navbar.value!.offsetWidth -
             navbarHorizontalPadding -
             (siteBrand.value?.offsetWidth || 0);
@@ -89,25 +144,19 @@ export default defineComponent({
       };
       handleLinksWrapWidth();
       window.addEventListener("resize", handleLinksWrapWidth, false);
+      window.addEventListener("orientationchange", handleLinksWrapWidth);
     });
 
     return {
+      canHide,
       navbar,
       siteBrand,
       siteBrandLink,
       siteBrandLogo,
+      siteBrandDarkLogo,
       siteBrandTitle,
       linksWrapperStyle,
     };
   },
 });
-
-function getCssValue(el: HTMLElement | null, property: string): number {
-  // NOTE: Known bug, will return 'auto' if style value is 'auto'
-  const val = el?.ownerDocument?.defaultView?.getComputedStyle(el, null)?.[
-    property as any
-  ];
-  const num = Number.parseInt(val as string, 10);
-  return Number.isNaN(num) ? 0 : num;
-}
 </script>
